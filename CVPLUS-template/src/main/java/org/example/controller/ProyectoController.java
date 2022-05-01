30d31c44474bfe3d3c169eed10da3d8e43a43fe3
Jeffrey Allison Fernandez <jeffreykaf@gmail.com>
2022-05-01 03:50:07 -0500

    Se sube UserStory3

--- CVPLUS-template/src/main/java/org/example/controller/ProyectoController.java
+++ CVPLUS-template/src/main/java/org/example/controller/ProyectoController.java
@@ -9,10 +9,13 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.example.util.Message;
 import org.example.business.PortafolioBusiness;
 import org.example.business.ProyectoBusiness;
 import org.example.entities.Portafolio;
 import org.example.entities.Proyecto;
+import org.primefaces.event.SelectEvent;
+
 
 @Named
 @SessionScoped
@@ -26,6 +29,7 @@ public class ProyectoController implements Serializable{
 	private PortafolioBusiness portafolioBusiness;
 	
 	private Proyecto proyecto;
+	private Proyecto proyectoSelected;
 	private List<Proyecto>proyectos;
 	
 	private Portafolio portafolio;
@@ -50,6 +54,7 @@ public class ProyectoController implements Serializable{
 	@PostConstruct
 	public void init() {
 		proyecto=new Proyecto();
+		proyectoSelected=new Proyecto();
 		proyectos=new ArrayList<>();
 		portafolio=new Portafolio();
 		portafolios=new ArrayList<>();
@@ -83,18 +88,61 @@ public class ProyectoController implements Serializable{
 			if (proyecto.getIdProyecto()!=null) {// si se cumple sera un update
 				proyecto.setPortafolio(portafolio);
 				proyectoBusiness.update(proyecto);
+				Message.messageInfo("Se actualizo el registro correctamente");
 			}else {//sino guardar
 				proyecto.setPortafolio(portafolio);
 				proyectoBusiness.insert(proyecto);
+				Message.messageInfo("Se guardo correctamente");
 			}
 			this.getAllProyectos();
 			this.resetForm();
 			view="listPro";
 		} catch (Exception e) {
+			Message.messageError("Error ProyectoController:"+e.getMessage());
 			// TODO: handle exception
 		}
 
 		return view;
+	}
+	
+	public String editProyecto() {
+		String view="";
+		try {
+			
+			if (this.proyectoSelected!=null) {// si se cumple sera habra seleccionado un elemento
+				this.proyecto=proyectoSelected;
+				this.portafolios=portafolioBusiness.getAllPortafolios();
+				view="/proyecto/updatePro";
+			}else {
+				Message.messageInfo("Por favor selecciona un producto");
+			}
+			
+			
+		} catch (Exception e) {
+			Message.messageError("Error ProyectoController:"+e.getMessage());
+		}
+		return view;
+	}
+	
+	
+	public Proyecto getProyectoSelected() {
+		return proyectoSelected;
+	}
+
+	public void setProyectoSelected(Proyecto proyectoSelected) {
+		this.proyectoSelected = proyectoSelected;
+	}
+
+	public ProyectoBusiness getProyectoBusiness() {
+		return proyectoBusiness;
+	}
+
+	public void setProyectoBusiness(ProyectoBusiness proyectoBusiness) {
+		this.proyectoBusiness = proyectoBusiness;
+	}
+
+	public void selectProyecto(SelectEvent e) {
+		this.proyectoSelected=(Proyecto) e.getObject();//Convierto un tipo de dato object en un tipo de dato producto, CAST
 	}
 	
 	public void resetForm() {
