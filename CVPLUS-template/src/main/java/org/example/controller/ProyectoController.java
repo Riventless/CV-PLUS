66c1cd003d2c127ac65f83e7baec3fc6b70ec169
Jeffrey Allison Fernandez <jeffreykaf@gmail.com>
2022-05-01 02:39:51 -0500

    Se sube UserStory2

--- CVPLUS-template/src/main/java/org/example/controller/ProyectoController.java
+++ CVPLUS-template/src/main/java/org/example/controller/ProyectoController.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.example.business.PortafolioBusiness;
 import org.example.business.ProyectoBusiness;
+import org.example.entities.Portafolio;
 import org.example.entities.Proyecto;
 
 @Named
+@SessionScoped
 public class ProyectoController implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -25,10 +28,31 @@ public class ProyectoController implements Serializable{
 	private Proyecto proyecto;
 	private List<Proyecto>proyectos;
 	
+	private Portafolio portafolio;
+	private List<Portafolio>portafolios;
+	
+	public Portafolio getPortafolio() {
+		return portafolio;
+	}
+
+	public void setPortafolio(Portafolio portafolio) {
+		this.portafolio = portafolio;
+	}
+
+	public List<Portafolio> getPortafolios() {
+		return portafolios;
+	}
+
+	public void setPortafolios(List<Portafolio> portafolios) {
+		this.portafolios = portafolios;
+	}
+
 	@PostConstruct
 	public void init() {
 		proyecto=new Proyecto();
 		proyectos=new ArrayList<>();
+		portafolio=new Portafolio();
+		portafolios=new ArrayList<>();
 		
 		getAllProyectos();
 	}
@@ -42,7 +66,44 @@ public class ProyectoController implements Serializable{
 		}
 	}
 	
+	public String newProyecto() {
+		try {
+			this.portafolios=portafolioBusiness.getAllPortafolios();
+			this.resetForm();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "insertPro.xhtml";
+	}
 	
+	public String saveProyecto() {
+		
+		String view="";
+		try {
+			if (proyecto.getIdProyecto()!=null) {// si se cumple sera un update
+				proyecto.setPortafolio(portafolio);
+				proyectoBusiness.update(proyecto);
+			}else {//sino guardar
+				proyecto.setPortafolio(portafolio);
+				proyectoBusiness.insert(proyecto);
+			}
+			this.getAllProyectos();
+			this.resetForm();
+			view="listPro";
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return view;
+	}
+	
+	public void resetForm() {
+		proyecto=new Proyecto();
+	}
+	
+	public String listProyecto() {
+		return "listPro.xhtml";
+	}
 	
 	public Proyecto getProyecto() {
 		return proyecto;
