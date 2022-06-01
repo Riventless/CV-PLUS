7811c8523a807dcbb2fcfecfe089804954943641
andresdolorier <85039785+andresdolorier@users.noreply.github.com>
2022-05-31 21:11:38 -0500

    Tb3
    USUARIOS: Empresa y asesor

--- CVPLUS-template/src/main/java/com/example/entities/Empresa.java
+++ CVPLUS-template/src/main/java/com/example/entities/Empresa.java
@@ -8,6 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
 
 @Entity
 @Table(name="Empresas")
@@ -15,22 +19,27 @@ public class Empresa {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long IdEmpresa;
-	
-	@Column(name = "Nombre_Empresa", length = 100, nullable = false)
+
+	@NotEmpty(message = "Debe ingresar nombre del doctor*")
+
+	@Column(name = "Nombre_Empresa", length = 55, nullable = false)
 	private String Nombre_Empresa;
 	
 	@Column(name = "Direccion_Empresa", length = 50, nullable = false)
-	private String Direccion_Empresa;
+	private String Direccion_Empresa; 
 	
+	@Size(min = 8, max = 8, message = "El numero de telefono tiene 8 digitos")
 	@Column(name = "NumTelf",nullable = false)
 	private Long NumTelf;
 	
 	@Column(name = "Email_Empresa", length = 50, nullable = false)
 	private String Email_Empresa;
 	
+	@Size(min = 8, max = 50, message = "El numero de RUC debe tener 20 digitos")
 	@Column(name = "Ruc",nullable = false)
-	private Long Ruc;
-
+	private String Ruc;
+	
+	@Size(min = 8, max = 20, message = "La contraseña debe tener minimo digitos")
 	@Column(name = "Contrasenia",length = 50, nullable = false)
 	private String Contrasenia;
 	
@@ -74,11 +83,13 @@ public class Empresa {
 		Email_Empresa = email_Empresa;
 	}
 
-	public Long getRuc() {
+
+
+	public String getRuc() {
 		return Ruc;
 	}
 
-	public void setRuc(Long ruc) {
+	public void setRuc(String ruc) {
 		Ruc = ruc;
 	}
 
