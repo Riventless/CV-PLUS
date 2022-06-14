7ba49fb1a362d1b6c32d172393819e33766579b8
Jeffrey Allison Fernandez <jeffreykaf@gmail.com>
2022-06-13 21:05:07 -0500

    Buscar Portafolio

--- CVPLUS-template/src/main/java/com/example/entities/Portafolio.java
+++ CVPLUS-template/src/main/java/com/example/entities/Portafolio.java
@@ -10,11 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name="portafolios" )
 public class Portafolio {
 	@Id
+<<<<<<< Updated upstream
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 	@Column(name="nombre",nullable=false)
@@ -23,13 +25,53 @@ public class Portafolio {
 	private String comentario;
 	@Column(name="valoracion",nullable=true)
 	private String valoracion;
+=======
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long IdPortafolio;
+	
+	@NotEmpty(message = "Ingresar el titulo del portafolio")
+	@Column(name = "Nombre_Porta", length = 50, nullable = false)
+	private String NombrePortafolio;
+	
+	@NotEmpty(message = "Ingresar la descripcion del portafolio")
+	@Column(name = "Descripcion_Portafolio", length =500, nullable = false)
+	private String DescripcionPortafolio;
+	
+	@ManyToOne
+	@JoinColumn(name="IdStudent",nullable=true)
+	private Student student;
+	
+	
+	public Portafolio() {
+	}
+	
+	public Portafolio(String NombrePortafolio) {
+		this.NombrePortafolio = NombrePortafolio;
+		
+	}
+	
+	
+>>>>>>> Stashed changes
 	
 	@ManyToMany(mappedBy="portafolios")
 	private List<Empresa> empresas=new ArrayList<>();
 	
 	
+<<<<<<< Updated upstream
 	public Long getId() {
 		return id;
+=======
+	public String getDescripcionPortafolio() {
+		return DescripcionPortafolio;
+	}
+
+	public void setDescripcionPortafolio(String descripcionPortafolio) {
+		DescripcionPortafolio = descripcionPortafolio;
+	}
+
+	public List<Proyecto> getProyectos() {
+		return proyectos;
+>>>>>>> Stashed changes
 	}
 	public void setId(Long id) {
 		this.id = id;
--- CVPLUS-template/src/main/java/com/example/repository/PortafolioRepository.java
+++ CVPLUS-template/src/main/java/com/example/repository/PortafolioRepository.java
@@ -1,6 +1,7 @@
 package com.example.repository;
 
 import java.util.List;
+<<<<<<< Updated upstream
 
 import com.example.entities.Portafolio;
 
@@ -18,3 +19,36 @@ public interface PortafolioRepository extends JpaRepository<Portafolio,Long>{
 	
 
 }
+=======
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.example.entities.Portafolio;
+
+public interface PortafolioRepository extends JpaRepository<Portafolio, Long> {
+
+	@Query("FROM Portafolio p WHERE p.IdPortafolio like :id")
+	List<Portafolio>findByIdPortafolio(@Param("id")Long IdPortafolio);
+	
+	@Query("FROM Portafolio p WHERE p.NombrePortafolio like :nombre")
+    List<Portafolio> findByTitleContaining(@Param("nombre") String nombre);
+	
+	@Query("FROM Portafolio p WHERE p.NombrePortafolio like :NombrePortafolio")
+    List<Portafolio> buscarPortafolio(@Param("NombrePortafolio") String NombrePortafolio);
+	
+	//@Query("SELECT p FROM Portafolio p  WHERE p.NombrePortafolio=?1")
+	//public List<Portafolio> buscarPortafolio(String NombrePortafolio);
+	
+	
+	
+	
+	@Query("FROM Portafolio p ORDER BY NombrePortafolio ASC")
+    public List<Portafolio>findAllSortByName();
+    	
+	
+
+    	
+    
+
+}
+>>>>>>> Stashed changes
