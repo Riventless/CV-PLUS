8b2af51d1e7ab9e65dd5d7c26c4914ab5d4c1f4a
VastyEsthef <vasty.terrel07@gmail.com>
2022-06-26 19:47:06 -0500

    Asesorias - Estudiante

---
+++ demo-TB3/src/main/java/com/example/controller/ServiceController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.example.service.ServicioXAdviserService;
+
+
+@Controller
+@RequestMapping("/advisors")
+public class ServiceController {
+	
+
+	/*
+	 * private ServicioXAdviserService servicio_serviceAdv;
+	 * 
+	 * public ServiceController(ServicioXAdviserService servicio_serviceAdv) {
+	 * this.servicio_serviceAdv = servicio_serviceAdv;
+	 * 
+	 * }
+	 * 
+	 * @GetMapping public String showAllServices_Advisors(Model model) {
+	 * model.addAttribute("serviceAdv_collection", servicio_serviceAdv.getAllSxA());
+	 * return "students/list_services_advisers"; }
+	 */
+    
+    
+
+}
